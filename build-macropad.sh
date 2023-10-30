a75ed2c5a9fbf62e5396bd4aa77fd35f8c064ba5
subhead <subhead@gmail.com>
2023-10-30 11:57:05 +0100

    changed path

--- build-macropad.sh
+++ build-macropad.sh
@@ -2,8 +2,8 @@
 
 set -x
 
-rm -rf /home/alex/qmk_firmware/keyboards/qmk/qmk_firmware/keyboards/keyboards/handwired/yaumpad/*
-rm -rf /home/alex/qmk_firmware/keyboards/qmk/qmk_firmware/users/subhead/*
+rm -rf /home/alex/dev/keyboards/qmk/qmk_firmware/keyboards/keyboards/handwired/yaumpad/*
+rm -rf /home/alex/dev/keyboards/qmk/qmk_firmware/users/subhead/*
 cp -r /home/alex/dev/keyboards/qmk/keymaps/handwired/yaumpad /home/alex/dev/keyboards/qmk/qmk_firmware/keyboards/handwired/yaumpad
 cp -r /home/alex/dev/keyboards/qmk/users/subhead /home/alex/dev/keyboards/qmk/qmk_firmware/users
 
